85566f0f8649b506ef296590195e167577957d3c
= <=>
2017-04-22 18:14:31 -0500

    attempt to add favicon

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-    console.log($)
-
     $('.email').on('click', function () {
         $('.email-label').addClass('hidden')
         $('.email-address').removeClass('hidden')
